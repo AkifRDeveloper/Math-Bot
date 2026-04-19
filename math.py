41a643f50fda57c873899133b77e21d1c4254315
AkifRDeveloper <legend.soccer10101@gmail.com>
2026-04-19 09:10:00 -0700

    Create math.py

---
+++ math.py
@@ -0,0 +1,50 @@
+import random
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def subtract(ctx, left: int, right: int):
+    """subtracts two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def multiply(ctx, left: int, right: int):
+    """multiplies two numbers together."""
+    await ctx.send(left * right)
+
+@bot.command()
+async def divide(ctx, left: int, right: int):
+    """divides two numbers together."""
+    await ctx.send(left / right)
+
+bot.run("")
+
+
+#put your token in 47
